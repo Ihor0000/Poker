8aab240034a7ee6e73f1aebc0be58acda5213db9
Ihor <igor.shishkyn@gmail.com>
2024-09-14 21:17:00 +0300

    10.6!!!!!

--- app.py
+++ app.py
@@ -22,6 +22,24 @@ round_names = ['Префлоп', 'Флоп', 'Тёрн', 'Ривер', 'Спас
 @app.route('/index.html')
 def baseindex():
     return render_template('index.html')
+@app.route('/profile.html')
+def profile():
+    return render_template('profile.html')
+@app.route('/tournaments.html')
+def tournaments():
+    return render_template('tournaments.html')
+
+@app.route('/rating.html')
+def rating():
+    return render_template('rating.html')
+
+@app.route('/rules.html')
+def rules():
+    return render_template('rules.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
 
 @app.route('/game.html')
 def game():
